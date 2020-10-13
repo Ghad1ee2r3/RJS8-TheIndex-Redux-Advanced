f4bfe0db8d7c473f9d3b07ca197752ede2ba9ec7
ghadeer <apps02977@gmail.com>
2020-10-13 23:19:33 +0300

    Redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,8 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import authors from "./authors";
 import books from "./books";
 import thunk from "redux-thunk";
-import authorsReducers from "./authors";
-import booksReducers from "./books";
+
 
 const rootReducer = combineReducers({
   authors: authors,
